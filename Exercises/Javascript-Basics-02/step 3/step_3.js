0501d0d8633fa9ff166542460533c5dbaa3179df
Your Name <your.email@example.com>
2019-05-20 18:40:01 +0300

    step_3

---
+++ Exercises/Javascript-Basics-02/step 3/step_3.js
@@ -0,0 +1,12 @@
+var x = document.createElement('div');
+var y= document.querySelector("div");
+document.body.appendChild(x);
+
+var z= document.querySelector("input");
+z.addEventListener("keyup",popcorn);
+function popcorn (){
+x.innerHTML=z.value;
+
+
+
+}
