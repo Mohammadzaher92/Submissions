176ec647e42215c260f17b545107570aec4206b3
Your Name <your.email@example.com>
2019-05-20 12:46:11 +0300

    step_5

---
+++ Exercises/Javascript-Basics-01/step_4.js
@@ -0,0 +1,4 @@
+var name = prompt("enter your name");
+var surname =prompt("enter your surname");
+var city = prompt("enter your city location");
+alert("name :" + name+ "\n"+ "surname :" + surname+ "\n" +"city :" +city);
---
+++ Exercises/Javascript-Basics-01/step_5.js
@@ -0,0 +1,5 @@
+var num1 =parseInt (prompt("enter number1"));
+var num2 =parseInt (prompt("enter number2"));
+alert(num1*num2);
+
+
