1dfa7dc9482d13e57c369dc98d997dc2d945db1b
Your Name <your.email@example.com>
2019-05-20 12:15:46 +0300

    step_2

--- Exercises/Javascript-Basics-01/step_2.js
+++ Exercises/Javascript-Basics-01/step_2.js
@@ -1,4 +1,4 @@
 var name = prompt("enter your name");
 var surname = prompt ("enter your surname");
 var city = prompt ("enter your city");
-alert("name :"+name \n"surname :"+surname \n"city :"+city)
+alert("name :" + name+ "\n"+ "surname :" + surname+ "\n" +"city :" +city);
