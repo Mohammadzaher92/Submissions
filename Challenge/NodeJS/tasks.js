4fb87520f1afea5154f39c6a0f87f97fefa6159c
Your Name <your.email@example.com>
2019-06-10 22:35:07 +0300

    change apps name

--- Challenge/NodeJS/tasks.js
+++ Challenge/NodeJS/tasks.js
@@ -10,155 +10,73 @@
  * @returns {void}
  */
 function startApp(name){
-  process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', onDataReceived);
-  console.log(`Welcome to ${name}'s application!`)
-  console.log("--------------------")
-}
-
-
-/**
- * Decides what to do depending on the data that was received
- * This function receives the input sent by the user.
- * 
- * For example, if the user entered 
- * ```
- * node tasks.js batata
- * ```
- * 
- * The text received would be "batata"
- * This function  then directs to other functions
- * 
- * @param  {string} text data typed by the user
- * @returns {void}
- */
-
-
-var array=['what is node.js','how you can use it.','try it yourself and never give up'];
-
-function onDataReceived(text) {
-  console.log(text)
-  var lenght = text.length;
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', onDataReceived);
+    console.log(`Welcome to ${name}'s application!`)
+    console.log("--------------------")
+  }
   
-  if (text === 'quit\n'){
-    quit();
-  }
-  else if (text.trim().slice(0,4)  === 'help'){
-    help();
-  }
-  else if(text === 'list\n') {
-    list();
-  }
-  else if(text.trim().slice(0,3)=== 'add'){
-    add(text,lenght);}
-
-  else if (text.trim().slice(0,6) === 'remove') {
-      remove(text);
-    }
-
-  else if (text === 'exit\n'){
-    quit();
-  }
-  else if(text.trim().slice(0,5) === 'hello'){
-    hello(text);
-    
-  }
-  else{
-    unknownCommand(text);
-  }
-}
-
-
-/**
- * prints "unknown command"
- * This function is supposed to run when all other commands have failed
- *
- * @param  {string} c the text received
- * @returns {void}
- */
-function unknownCommand(c){
-  console.log('unknown command: "'+c.trim()+'"')
-}
-
-
-/**
- * Says hello
- *
- * @returns {void}
- */
-function hello(t){
-  console.log('hello'+t.trim().substring(5,t.length)+'!')
-}
-
-
-/**  var lenght = text.length;
-
- * Exits the application
- *
- * @returns {void}
- */
-function quit(){
-  console.log('Quitting now, goodbye!')
-  process.exit();
-}
-/**
- * help list
- *
- * @returns {void}
- */
-
-function help (){
-  //console.log( 'UnkownCommand\n' + 'hello\n' + 'quit\n');
-  console.log('You have seven commads:\n1-hello\n2-quit\n3-exit\n4-list\n5-add\n6-remove\n7-help')
-
-}
-/**
- * list function
- * Show all the tasks that you should be done!!!
- *
- * @returns {void}
- */
-function list(text){
-  for (var i=0 ;i<array.length;i++){
-    console.log((i+1)*1+'-'+array[i])
-  }}
+  
   /**
- * add function
- *
- * @returns {void}
- */
-  function add(x,l){
-    if (l>5){
-      array.push(x.trim().substring(4,l))
+   * Decides what to do depending on the data that was received
+   * This function receives the input sent by the user.
+   * 
+   * For example, if the user entered 
+   * ```
+   * node tasks.js batata
+   * ```
+   * 
+   * The text received would be "batata"
+   * This function  then directs to other functions
+   * 
+   * @param  {string} text data typed by the user
+   * @returns {void}
+   */
+  function onDataReceived(text) {
+    if (text === 'quit\n') {
+      quit();
+    }
+    else if(text === 'hello\n'){
+      hello();
     }
     else{
-      console.log('error')
+      unknownCommand(text);
     }
-    
   }
+  
   
   /**
- * remove function
- * you can remove any task you add in addtion to the basic tasks!!
- *
- * @returns {void}
- */
-function remove(n){
-  var number = n.charAt(7);
-  if( n=='remove\n'){
-    array.splice(array.length-1,1)
+   * prints "unknown command"
+   * This function is supposed to run when all other commands have failed
+   *
+   * @param  {string} c the text received
+   * @returns {void}
+   */
+  function unknownCommand(c){
+    console.log('unknown command: "'+c.trim()+'"')
   }
-  else if(number>array.length){
-    console.log('Please enter the right number of task to remove')
-  }
-  else{
-  for (number;number<array.length+1;number++){
-    array.splice(number-1,1)
-    break;
-  }}
-}
   
-
-// The following line starts the application
-startApp("Mohammad Zaher Alnajjar")
+  
+  /**
+   * Says hello
+   *
+   * @returns {void}
+   */
+  function hello(){
+    console.log('hello!')
+  }
+  
+  
+  /**
+   * Exits the application
+   *
+   * @returns {void}
+   */
+  function quit(){
+    console.log('Quitting now, goodbye!')
+    process.exit();
+  }
+  
+  // The following line starts the application
+  startApp("zaher alnajjar")
